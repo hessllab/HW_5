e2455a0d56d7fc3a1a40432b13aa4182b6ad9083
sis0004 <54180343+sis0004@users.noreply.github.com>
2019-09-18 07:41:44 -0400

    Upload state.sh

---
+++ state.sh
@@ -0,0 +1,10 @@
+mkdir -p ITRDB_by_state/"$1"
+cd ITRDB_by_state/"$1"
+wget -r -e robots=off -A "$1"'*.rwl' -R "$1"'*-noaa.rwl' -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+
+for sitename in "$1"*.rwl
+do
+  echo $sitename | cut -d '.' -f 1 >> "$1"_sites.txt
+  head -n 1 $sitename >> "$1"_sites.txt
+  echo  >> "$1"_sites.txt
+done
