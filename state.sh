9528815671413822000f90246ffd0129b4f996bf
HartfordJ <54254247+HartfordJ@users.noreply.github.com>
2019-09-18 18:25:05 -0400

    Update state.sh

--- state.sh
+++ state.sh
@@ -1,4 +1,11 @@
-for state in *.rwl
+mkdir "$1"output
+cd "$1"output
+wget -r -e robots=off -A "$1"???.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+wget -r -e robots=off -A "$1"????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+wget -r -e robots=off -A "$1"?????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+
+for filename in *.rwl
 do
-	ls $state | cut -d . -f 1 $state | head -n 1 $state >> state_output.txt
+  ls "$filename" | cut -d . -f 1 $filename >> "$1"_sites.txt
+  head -n 1 "$filename" >> "$1"_sites.txt
 done
