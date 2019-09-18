a932087e4efc67fa9e0e0abe49ac799ddcf2a52e
Tobi Ore <54361683+tobi-ore@users.noreply.github.com>
2019-09-17 20:48:29 -0400

    Update state.sh

--- state.sh
+++ state.sh
@@ -4,9 +4,12 @@
 
 mkdir "$1" && cd "$1"       #creates and changes the working directory to a new directory called 'state_abbrev'
 
+#retrieve .rwl files for specified state from the database excluding files with noaa suffix in their name
 wget -r -e robots=off -A "$1"*.rwl -np -nd -q -R "*noaa.rwl" https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+
 for sitename in *.rwl 
     do 
-        echo $sitename | cut -d . -f 1 >> "$1"_name.txt
-        head -n 1 $sitename >> "$1"_name.txt 
+        echo $sitename | cut -d . -f 1 >> "$1"_sites.txt   #writes the name of the .rwl without the extension to a textfile named 'state_abbrev'_sites
+        
+        head -n 1 $sitename >> "$1"_sites.txt #writes the first line of the .rwl file to the textfile
 done
