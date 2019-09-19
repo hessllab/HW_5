b3afdf72aae9436c8692d07affcf4e734d16bc5b
ahessl <ahessl@users.noreply.github.com>
2019-09-19 14:56:49 -0400

    tiny format

--- state.sh
+++ state.sh
@@ -5,7 +5,7 @@ cd "$1"_trees
 wget -r -e robots=off -A "$1"???.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
 wget -r -e robots=off -A "$1"????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
 for filename in *.rwl
- do
+do
   ls "$filename" | cut -c 1-5 >> "$1"_sites.txt
   head -n 1 "$filename" >> "$1"_sites.txt
- done
+done
