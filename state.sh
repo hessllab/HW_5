5a06e7a5e94a861e45635b438288f7f54b3cea48
HartfordJ <54254247+HartfordJ@users.noreply.github.com>
2019-09-18 21:56:54 -0400

    Update state.sh

--- state.sh
+++ state.sh
@@ -3,6 +3,7 @@ cd "$1"output
 wget -r -e robots=off -A "$1"???.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
 wget -r -e robots=off -A "$1"????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
 wget -r -e robots=off -A "$1"?????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+wget -r -e robots=off -A "$1"??????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
 
 for filename in *.rwl
 do
