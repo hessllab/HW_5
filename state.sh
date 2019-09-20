d98fe390c9b38549c599e4ccb1ce97e7cb0d65c1
HartfordJ <54254247+HartfordJ@users.noreply.github.com>
2019-09-20 19:07:55 -0400

    current

--- state.sh
+++ state.sh
@@ -1,12 +1,8 @@
 mkdir "$1"output
 cd "$1"output
-wget -r -e robots=off -A "$1"???.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
-wget -r -e robots=off -A "$1"????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
-wget -r -e robots=off -A "$1"?????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
-wget -r -e robots=off -A "$1"??????.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
+wget -r -e robots=off -A "$1"*.rwl -np -nd https://www1.ncdc.noaa.gov/pub/data/paleo/treering/measurements/northamerica/usa/
 
-for filename in *.rwl
+for state in *.rwl
 do
-  ls "$filename" | cut -d . -f 1 $filename >> "$1"_sites.txt
-  head -n 1 "$filename" >> "$1"_sites.txt
+	ls $state | find data -name '*s.txt' | grep -v -noaa | head -n 1 $state >> output.txt
 done
